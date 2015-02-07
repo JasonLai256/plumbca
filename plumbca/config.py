8946622a049aacc4962a30bb8a8ba56d9398e17d
Jason Lai <jasonlai256@gmail.com>
2015-02-08 00:57:50 +0800

    add Config class and PlumbcaConfigNotFound class.

---
+++ plumbca/config.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+# Copyright (c) 2015 jason lai
+#
+# See the file LICENSE for copying permission.
+
+from configparser import ConfigParser
+
+from .exception import PlumbcaConfigNotFound
+
+
+defaults = {
+    'global': {
+        'daemonize': 'no',
+        'pidfile': '/var/run/plumbca.pid',
+        'bind': '127.0.0.1',
+        'port': '4273',
+        'logfile': '/var/log/plumbca.log',
+        'dumpfilename': 'dump.save',
+        'dumpdir': '/var/lib/plumbca/',
+    },
+}
+
+
+class Config(dict):
+
+    def __init__(self, section):
+        super().__init__()
+        self._section = section
+        self.update(defaults[section])
+
+    def readFrom(self, f):
+        config = ConfigParser()
+
+        rv = config.read(f)
+        if not rv:
+            raise PlumbcaConfigNotFound('Failed to read the config file {}'.format(f))
+
+        self.update({k: v for k, v in config.items(self._section)})
+
+
+DefaultConf = Config('global')
