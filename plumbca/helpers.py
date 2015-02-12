3a670ffabf8ab4073ff8080085b5a6f62bee83dd
Jason Lai <jasonlai256@gmail.com>
2015-02-12 21:37:08 +0800

    refactor the indexing helper functions and add relative unittest cases.

--- plumbca/helpers.py
+++ plumbca/helpers.py
@@ -1,25 +1,38 @@
 # -*- coding:utf-8 -*-
+"""
+    plumbca.helpers
+    ~~~~~~~~~~~~~~~
 
-# Copyright (c) 2015 jason lai
-#
-# See the file LICENSE for copying permission.
+    Implements various helpers.
+
+    :copyright: (c) 2015 by Jason Lai.
+    :license: BSD, see LICENSE for more details.
+"""
 
 from bisect import bisect_left
 
 
-def find_ge(a, x):
+def find_eq(a, x, ret_index=False):
+    '''Find the leftmost item exactly equal to x'''
+    i = bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        return i if ret_index else a[i]
+    raise ValueError('Not found the value that == {}\n'.format(x))
+
+
+def find_ge(a, x, ret_index=False):
     '''Find leftmost item greater than or equal to x'''
     i = bisect_left(a, x)
     if i != len(a):
-        return a[i]
+        return i if ret_index else a[i]
     raise ValueError('Not found the value that >= {}\n'
                      '(The maximun item is {})'.format(x, a[-1]))
 
 
-def find_lt(a, x):
+def find_lt(a, x, ret_index=False):
     '''Find rightmost value less than x'''
     i = bisect_left(a, x)
     if i:
-        return a[i-1]
+        return i-1 if ret_index else a[i-1]
     raise ValueError('Not found the value that < {} \n'
                      '(The minimun item is {})'.format(x, a[0]))
---
+++ tests/test_helpers.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+"""
+    tests.helpers
+    ~~~~~~~~~~~~~
+
+    :copyright: (c) 2015 by Jason Lai.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import pytest
+
+from plumbca.helpers import find_ge, find_lt, find_eq
+from data import metadata_t
+
+
+def test_find_func():
+    '''testing the helper functions that using bisect mosule.'''
+    # [1] ->
+    item = [143373]
+    assert find_ge(metadata_t, item) == [168011, 'hello', 123]
+    assert find_ge(metadata_t, item, True) == 2
+    assert find_lt(metadata_t, item) == [143372, 'hello', 1234]
+    assert find_lt(metadata_t, item, True) == 1
+    with pytest.raises(ValueError):
+        find_eq(metadata_t, item)
+
+    # [2] ->
+    item = [143372]
+    assert find_ge(metadata_t, item) == [143372, 'hello', 123]
+    assert find_ge(metadata_t, item, True) == 0
+    with pytest.raises(ValueError):
+        find_lt(metadata_t, item)
+    with pytest.raises(ValueError):
+        find_eq(metadata_t, item)
+
+    # [3] ->
+    item = [228072]
+    assert find_ge(metadata_t, item) == [228072, 'hello', 123]
+    assert find_ge(metadata_t, item, True) == 5
+    assert find_lt(metadata_t, item) == [188072, 'hello', 123]
+    assert find_lt(metadata_t, item, True) == 4
+    with pytest.raises(ValueError):
+        find_eq(metadata_t, item)
+
+    # [4] ->
+    item = [228072, 'hello', 123]
+    assert find_ge(metadata_t, item) == [228072, 'hello', 123]
+    assert find_ge(metadata_t, item, True) == 5
+    assert find_lt(metadata_t, item) == [188072, 'hello', 123]
+    assert find_lt(metadata_t, item, True) == 4
+    assert find_eq(metadata_t, item) == [228072, 'hello', 123]
+    assert find_eq(metadata_t, item, True) == 5
+
+    # [5] ->
+    item = [558072]
+    with pytest.raises(ValueError):
+        rv = find_ge(metadata_t, item)
+    assert find_lt(metadata_t, item) == [228072, 'hello', 123]
+    assert find_lt(metadata_t, item, True) == 5
+    with pytest.raises(ValueError):
+        rv = find_eq(metadata_t, item)
